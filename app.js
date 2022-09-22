111815d8dc94a294c87b7181d5d8f5fe80a86101
Noah Kerstiens <44041365+nkerstiens@users.noreply.github.com>
2022-09-22 13:19:27 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://noahkerstiens:Mag00329!@cluster-op.m3cyrwk.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
